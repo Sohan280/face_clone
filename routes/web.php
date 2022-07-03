8d1deedc723f8625fadeb56d8791efd6b522a316
sohan_khan <khansohan280@gmail.com>
2022-07-04 03:23:23 +0600

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,4 +49,8 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::group(['middleware'=> 'auth'], function(){
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    Route::resource('/posts', PostController::class);
+});
